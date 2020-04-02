6c2fe144ec288b87f40efd363571adb2a6598e26
William Myers <mwilliammyers@gmail.com>
2020-04-02 15:26:30 -0600

    Add ConversationDataset & MessageDataset

--- chitchat_dataset/__init__.py
+++ chitchat_dataset/__init__.py
@@ -1,7 +1,7 @@
 """Chit Chat Challenge dataset."""
 import json
 import os
-from typing import Optional
+from typing import Iterator, Optional
 
 
 def _default_dataset_path() -> str:
@@ -18,22 +18,46 @@ class Dataset(dict):
         super(Dataset, self).__init__(json.load(open(self.path)))
 
 
-class FlatDataset:
+class ConversationDataset:
+    """Chit Chat Challenge dataset.
+
+    The dataset is an iterator over conversations;
+    e.g. `[["message1", "message2", ...], ...]`.
     """
-    Chit Chat Challenge dataset as a flat iterator over conversations, with
-    optional end of utterance and end of message tokens.
+
+    def __init__(self, path: str = None, end_of_utterance_token: str = " ") -> None:
+        """Instantiate a new ConversationDataset object."""
+        self.path = path if path is not None else _default_dataset_path()
+        self.eou_token = end_of_utterance_token
+        self._data = json.load(open(self.path))
+
+    def __iter__(self) -> Iterator[Iterator]:
+        """Iterate over conversations."""
+        for conv in self._data.values():
+            yield (self.eou_token.join(u["text"] for u in m) for m in conv["messages"])
+
+
+class MessageDataset:
+    """Chit Chat Challenge dataset.
+
+    The dataset is an iterator over messages for all conversation as a flat list;
+    e.g. `["message1", "message2", ...]`.
     """
 
     def __init__(
-        self, path=None, end_of_utterance_token="<EOU>", end_of_message_token="<EOM>"
-    ):
+        self,
+        path: str = None,
+        end_of_utterance_token: str = " ",
+        end_of_message_token: str = "<EOM>",
+    ) -> None:
+        """Instantiate a new MessageDataset object."""
         self.path = path if path is not None else _default_dataset_path()
         self.eou_token = end_of_utterance_token
         self.eom_token = end_of_message_token
         self._data = json.load(open(self.path))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator:
+        """Iterate over messages."""
         for conv in self._data.values():
-            yield self.eom_token.join(
-                self.eou_token.join(u["text"] for u in m) for m in conv["messages"]
-            )
+            for message in conv["messages"]:
+                yield self.eou_token.join(u["text"] for u in message)
